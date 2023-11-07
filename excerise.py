3e97ad53f6f2691b2a896875d3a7dda610586e5f
smritidhimal <135810855+smritidhimal@users.noreply.github.com>
2023-11-07 14:57:01 +0545

    Add files via upload

---
+++ excerise.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+print("hello smriti")
+
+c=38.4                                                                                              
+f=(c*(9/5))+32
+print(f)
+
+batted=1014
+not_out=164
+total_run=48426
+innings=batted-not_out
+avg=total_run/innings
+print(avg)
+
+group_size=24
+class_size=113
+full_groups=class_size//group_size
+left_overs=class_size%group_size
+print("no. of full groups:",full_groups)
+print("left over students:",left_overs)
